3309169f71e2a76d3d31cddb3eefdb10b0a93009
hj <unclebean2013@gmail.com>
2025-06-21 16:15:27 +0800

    update project mock files

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -1,17 +1,19 @@
 package org.demo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+public class Main {
+  public static void main(String[] args) {
+    WireMockServer server =
+        new WireMockServer(options().port(8080).usingFilesUnderDirectory("src/main/resources"));
+
+    server.start();
+
+    System.out.println("WireMock running at http://localhost:8080");
+  }
 }
